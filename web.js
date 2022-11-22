d3ecef6abfac61075e4bebae03a04b95bc7a94d7
ronit019u <110216859+ronit019u@users.noreply.github.com>
2022-11-22 19:06:27 +0530

    etch-a-skleleton main

--- web.js
+++ web.js
@@ -1,13 +1,56 @@
-const container= document.querySelector('container');
-const grids () => {
-for(let i = 1; i <=16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('grid_row');
-    for(let j = 1; j <=16; j++) {
-      const column = document.createElement('div');
-      column.classList.add('column_grid');
-      row.appendChild(column);
-    }
-    row.appendChild(container);
- }
+let color = "Black";
+document.addEventListener("DOMContentLoaded", function(){
+  createBoard(16);
+  let btn = document.querySelector("#popup");
+  btn.addEventListener("click", function(){
+   let size = getSize()
+   createBoard(size);
+  })
+                                                                                                   
+
+
+})
+  const createBoard = (size) => {
+  let board = document.querySelector(".board");
+
+board.style.gridTemplateColumns = 'repeat(16, 1fr)';
+board.style.gridTemplateRows = 'repeat(16, 1fr)';
+
+  let numDivs = size * size;
+
+for(let i = 0; i < numDivs; i++) {
+  let div = document.createElement("div");
+  div.addEventListener("mouseover", colorDiv)
+  board.insertAdjacentElement("beforeend", div);
+
 }
+}
+
+const getSize = (size) => {
+  let input = prompt("plz put size");
+  let message = document.querySelector("#message");
+  if(input == "") {
+   message.innerHTML = "plz send numbers"
+  } else if(input < 0 || input > 100 ) {
+    message.innerHTML = "plz send correct size"
+   } else {       
+    message.innerHTML = "Now you can play"
+    return input;
+   }
+ 
+}
+    function colorDiv() {
+    if(color == "random") {
+      this.style.backgroundColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6)
+        } else {
+          this.style.backgroundColor = 'black'
+        }
+
+  }
+     const setColor = (colorChoice) => {
+    color = colorChoice;
+  }
+   function  resetBoard() {
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor ="white")
+  }
